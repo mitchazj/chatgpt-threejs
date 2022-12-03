79ad14f5167e79667ffa23e2eaa74ec56d27b6a6
Mitchell Johnson <mjohnson@canva.com>
2022-12-03 01:48:21 +0000

    better lighting

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ document.body.appendChild(renderer.domElement);
 // create a pyramid-shaped geometry using the THREE.CylinderGeometry class
 var geometry = new THREE.CylinderGeometry(0, 1, 1, 4, 1, true);
 
-// create a material for the pyramid using the THREE.MeshBasicMaterial class
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// create a material for the pyramid using the THREE.MeshLambertMaterial class
+var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 
 // create a mesh for the pyramid using the THREE.Mesh class and the geometry and material created above
 var pyramid = new THREE.Mesh(geometry, material);
+
+// position the pyramid in front of the camera, with a positive z-coordinate
+pyramid.position.z = 1;
 
 // create a parent object for the pyramid mesh using the THREE.Object3D class
 var parent = new THREE.Object3D();
@@ -26,6 +29,30 @@ parent.add(pyramid);
 
 // add the parent object to the scene
 scene.add(parent);
+
+// create a point light using the THREE.PointLight class
+var pointLight = new THREE.PointLight(0xffffff, 1, 100);
+
+// position the point light so that it illuminates the pyramid
+pointLight.position.set(1, 1, 1);
+
+// add the point light to the scene
+scene.add(pointLight);
+
+// create a directional light using the THREE.DirectionalLight class
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+// position the directional light so that it illuminates the pyramid
+directionalLight.position.set(-1, -1, -1);
+
+// add the directional light to the scene
+scene.add(directionalLight);
+
+// create an ambient light using the THREE.AmbientLight class
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+// add the ambient light to the scene
+scene.add(ambientLight);
 
 // position the camera so that it points towards the pyramid
 camera.position.z = 5;
